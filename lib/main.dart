2d4f0db4ab2dfe68a050f8dd99cf10f683de30e9
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-29 22:29:53 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:spendwise/widgets/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
