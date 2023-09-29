2b7f932fb69090d0437763eae7b2eb1230f631e1
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-29 22:30:37 +0530

    created the main screen for displaying expenses

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:spendwise/widgets/expenses_list/expenses_list.dart';
+import 'package:spendwise/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Burger',
+      amount: 29.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Flutter Course 3',
+      amount: 100,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
