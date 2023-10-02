5a3d69600fe4a5d36d7b406fdcc2cf63cedf7df9
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-02 13:28:44 +0530

    Added swipe to delete feature

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,22 @@ import 'package:spendwise/models/expense.dart';
 import 'package:spendwise/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.removeExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) removeExpense;
 
   @override
   Widget build(context) {
     return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index],)
-    );
+        itemCount: expenses.length,
+        itemBuilder: (ctx, index) => Dismissible(
+              onDismissed: (direction) {
+                removeExpense(expenses[index]);
+              },
+              key: ValueKey(expenses[index]),
+              child: ExpenseItem(expenses[index]),
+            ));
   }
 }
