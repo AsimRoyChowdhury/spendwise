6289f3a1fe3deeb4001066d23494db817fefed55
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-04 22:24:39 +0530

    Added Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:spendwise/widgets/expenses.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 61, 51, 132),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 47, 45, 65),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: const Color.fromARGB(255, 58, 54, 111),
+          foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: const Color.fromARGB(255, 80, 76, 110),
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          elevation: 8,
+          shadowColor: const Color.fromARGB(255, 2, 2, 2),
+        ),
+        bottomSheetTheme: const BottomSheetThemeData().copyWith(
+          backgroundColor: const Color.fromARGB(255, 129, 123, 181),
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
