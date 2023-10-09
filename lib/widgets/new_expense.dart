89cee32980a2e3d59991e730ebf4a8c47a7c7012
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-09 23:12:00 +0530

    Added Category enum

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:spendwise/models/category.dart';
 import 'package:spendwise/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
